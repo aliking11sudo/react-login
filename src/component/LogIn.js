854d53442825eb507734248c7f816092b6568ec1
aliking11sudo <aliesfandani1382@gmail.com>
2023-10-21 09:36:45 +0330

    react-test

--- src/component/LogIn.js
+++ src/component/LogIn.js
@@ -1,20 +1,22 @@
 import NavBar from "./NavBar";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Card from "./Card";
 const LogIn = () => {
-
   return (
-    <div className="container-fluid">
+    <div>
+      <NavBar></NavBar>
+      <div className="container-fluid">
       <div className="row justify-content-center vh-100 align-items-center">
-        <NavBar></NavBar>
         <div className="col-12 col-lg-4">
           <Card />
         </div>
       </div>
     </div>
+    </div>
+  
   );
 };
 export default LogIn;
